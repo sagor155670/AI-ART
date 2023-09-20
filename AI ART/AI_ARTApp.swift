d0e7240543f76c1afb4867b01100401fcc0f9246
sagor155670 <sagor@braincraftapps.com>
2023-09-20 12:09:55 +0600

    Initial Commit

---
+++ AI ART/AI_ARTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ARTApp.swift
+//  AI ART
+//
+//  Created by Saiful Islam Sagor on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AI_ARTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
